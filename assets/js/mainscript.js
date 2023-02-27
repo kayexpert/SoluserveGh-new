e9dd46402f1ba39c48244c9ff2628f77edef7294
kayexpert <niikelvinkojotogbor@gmail.com>
2023-02-27 11:29:34 +0000

    update

--- assets/js/mainscript.js
+++ assets/js/mainscript.js
@@ -27,7 +27,25 @@ $(".partners-slide").owlCarousel({
 });
 
 // Navigation
+let mobile_nav_icon = document.querySelector(".mobile-nav-btn");
+let mobile_nav_bar = document.querySelector(".navbar");
+mobile_nav_icon.addEventListener("click", () => {
+  // mobile_nav_bar.classList.toggle("open");
+  mobile_nav_icon.classList.toggle("clicked");
+});
 window.addEventListener("scroll", () => {
   let navbar = document.querySelector(".nav-bar");
+
   navbar.classList.toggle("sticky", window.scrollY > 8);
+
+  let logo_element = document.querySelector(".nav-logo");
+  logo_element.src = "assets/img/Soluserv Logo.png";
+  if (window.scrollY > 8) {
+    // set the new image source
+    logo_element.src = "assets/img/Soluserv Logo B.png";
+  } else {
+    // set the initial image source
+    logo_element = logo_element.src;
+  }
 });
+// assets/img/Soluserv Logo.png
